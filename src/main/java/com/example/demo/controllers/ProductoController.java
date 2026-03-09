65d8940c3367dd4e218dd5e299195252833d5e58
Matias Fauda <matiasfauda@gmail.com>
2026-03-08 22:51:59 -0300

    Metodos del Controller

---
+++ src/main/java/com/example/demo/controllers/ProductoController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controllers;
+
+import com.example.demo.entities.Producto;
+import com.example.demo.repositories.ProductoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController //Indica que esta clase es un punto de entrada para peticiones HTTP
+@RequestMapping("/productos") //La url base va a ser http://localhost:8080/productos
+
+public class ProductoController {
+
+    @Autowired // Le pide a spring que "inyecte" el repo automaticamente
+    private ProductoRepository productoRepository;
+
+    //Obtengo los productos de la db
+    @GetMapping
+    public List<Producto> obtenerTodos(){
+        return productoRepository.findAll();
+    }
+    //Guardamos producto nuevo desde el navegador
+    @PostMapping
+    public Producto guardarProducto(@RequestBody Producto producto){
+        return productoRepository.save(producto);
+    }
+}
