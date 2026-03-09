4427d3127f819fc5e0592067ff8ea1da6395e300
Matias Fauda <matiasfauda@gmail.com>
2026-03-09 19:44:01 -0300

    Id de Categoria y modificacion de DataSeeder

--- src/main/java/com/example/demo/config/DataSeeder.java
+++ src/main/java/com/example/demo/config/DataSeeder.java
@@ -1,20 +1,43 @@
 package com.example.demo.config;
+
+import com.example.demo.entities.Categoria;
 import com.example.demo.entities.Producto;
+import com.example.demo.repositories.CategoriaRepository;
 import com.example.demo.repositories.ProductoRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Configuration
+
 public class DataSeeder {
     @Bean
-    CommandLineRunner initDatabase(ProductoRepository repository) {
+    CommandLineRunner initDatabase(ProductoRepository productoRepository, CategoriaRepository categoriaRepository) {
         return args -> {
-            // Solo insertamos si la base está vacía
-            if (repository.count() == 0) {
-                repository.save(new Producto(null,"Monitor 144hz", 300.0 , "Ideal para gaming"));
-                repository.save(new Producto(null,"Mouse Logi", 50.0 , "Sensor óptico"));
-                System.out.println("Base de datos inicializada con productos de prueba.");
-            }
+            System.out.println(">>> SEEDER: Iniciando la carga de datos...");
+            // 1. Creamos y guardamos la Categoria
+            Categoria c1 = new Categoria(null, "Electronica");
+            Categoria c2 = new Categoria(null,"Hogar");
+            categoriaRepository.save(c1);
+            categoriaRepository.save(c2);
+
+            // 2. Ahora creamos los productos asociados a esa categorias
+            Producto p1 = new Producto();
+            p1.setNombre("Monitor 144 hz");
+            p1.setPrecio(300.0);
+            p1.setDescripcion("Ideal para jugar");
+            p1.setCategoria(c1); //Aca le asocio la categoria
+
+            Producto p2 = new Producto();
+            p2.setNombre("Silla Oficina");
+            p2.setPrecio(150.0);
+            p2.setDescripcion("Ergonomica");
+            p2.setCategoria(c2);
+
+            productoRepository.save(p1);
+            productoRepository.save(p2);
+
+            System.out.println("Base de daatos inicializada con categoria y productos");
         };
     }
 }
--- src/main/java/com/example/demo/entities/Categoria.java
+++ src/main/java/com/example/demo/entities/Categoria.java
@@ -1,8 +1,6 @@
 package com.example.demo.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Entity
@@ -14,6 +12,7 @@ import lombok.*;
 
 public class Categoria {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nombre;
 }
