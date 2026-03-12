600a041078f06e59a0192335094a7ae1ef721004
Matias Fauda <matiasfauda@gmail.com>
2026-03-12 12:58:23 -0300

    Insercion de datos solo con Postman

--- src/main/java/com/example/demo/config/DataSeeder.java
+++ src/main/java/com/example/demo/config/DataSeeder.java
@@ -14,30 +14,8 @@ public class DataSeeder {
     @Bean
     CommandLineRunner initDatabase(ProductoRepository productoRepository, CategoriaRepository categoriaRepository) {
         return args -> {
-            System.out.println(">>> SEEDER: Iniciando la carga de datos...");
-            // 1. Creamos y guardamos la Categoria
-            Categoria c1 = new Categoria(null, "Electronica");
-            Categoria c2 = new Categoria(null,"Hogar");
-            categoriaRepository.save(c1);
-            categoriaRepository.save(c2);
-
-            // 2. Ahora creamos los productos asociados a esa categorias
-            Producto p1 = new Producto();
-            p1.setNombre("Monitor 144 hz");
-            p1.setPrecio(300.0);
-            p1.setDescripcion("Ideal para jugar");
-            p1.setCategoria(c1); //Aca le asocio la categoria
-
-            Producto p2 = new Producto();
-            p2.setNombre("Silla Oficina");
-            p2.setPrecio(150.0);
-            p2.setDescripcion("Ergonomica");
-            p2.setCategoria(c2);
-
-            productoRepository.save(p1);
-            productoRepository.save(p2);
-
-            System.out.println("Base de daatos inicializada con categoria y productos");
+            // Al dejarlo vacío, no se inserta nada, pero no borras la estructura
+            System.out.println("Base de datos lista (sin inserciones automáticas)");
         };
     }
 }
