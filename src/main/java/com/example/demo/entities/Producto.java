8c05a15ec315606777d14a892e12b9184aae148c
Matias Fauda <matiasfauda@gmail.com>
2026-03-19 16:40:16 -0300

    Validaciones de entidad Producto utilizando paquete Validation y sus anotaciones

--- src/main/java/com/example/demo/entities/Producto.java
+++ src/main/java/com/example/demo/entities/Producto.java
@@ -1,6 +1,9 @@
 package com.example.demo.entities;
 import jakarta.persistence.*;
 import lombok.*;
+//Paquete para validaciones con anotacion
+import jakarta.validation.constraints.*;
+
 @Entity //Le dice a JPA que esta clase es una tabla de la db
 @Table(name = "productos") //Defino el nombre de la tabla
 @Getter // Genera los getters
@@ -14,10 +17,15 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY) //El id se genera solo
     private Long id;
 
+    @NotBlank(message = "El nombre no puede estar vacio")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
     @Column(name = "nombre_producto", nullable = false, length = 100)
     private String nombre;
+
+    @NotNull(message = "El precio es obligatorio")
+    @Min(value = 0, message = "El precio debe ser mayor a 0")
     private Double precio;
-    private String Descripcion;
+    private String descripcion;
 
     // Aca tenemos la relacion
     @ManyToOne //Muchos productos pertenecen a UNA SOLA Categoria
